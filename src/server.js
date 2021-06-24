5750c43e88cc6d0f0c3c9bf495b7d51f9a8ecd62
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-06-24 10:36:28 -0300

    :ok: Configurando a conexão com mongodb utilizando o mongoose

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 import routes from './routes'
+
+dotenv.config()
 
 const server = express()
 const port = process.env.PORT || 3000
+
+mongoose.connect(
+  'mongodb+srv://SirGuiL21:28102002@cluster0.ifeyy.mongodb.net/bloggy-api?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
 
 server.use(express.json())
 server.use(routes)
