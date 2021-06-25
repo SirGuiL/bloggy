8ba55824fce2b22202346938a62674984bb87599
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-06-25 17:01:27 -0300

    :exclamation: Autenticação de login com JWT

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import UserController from './controller/UserController'
+import SessionController from './controller/SessionController'
 
 const routes = express.Router()
 
 routes.post('/users', UserController.store)
+
+routes.post('/auth', SessionController.store)
 
 export default routes
