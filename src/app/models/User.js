640e4fd2db55460233a75e415be868d7b9bb346e
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-06-29 23:10:23 -0300

    :ballot_box_with_check: Criando model e controller de posts

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -19,6 +19,12 @@ const UserSchema = new Schema(
       type: String,
       required: true,
     },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
   },
   { timestamps: true }
 )
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import UserController from './app/controller/UserController'
 import SessionController from './app/controller/SessionController'
+import PostController from './app/controller/PostController'
 import authMiddleware from './app/middlewares/authentication'
 
 const routes = express.Router()
@@ -12,5 +13,5 @@ routes.use(authMiddleware)
 
 routes.put('/users', UserController.update)
 routes.delete('/users', UserController.delete)
-
+routes.post('/posts', PostController.store)
 export default routes
